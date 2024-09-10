a94045570ec5cf6be9c6ca8e648eb0b41a548316
marco magliani <maglianim@gmail.com>
2024-09-10 15:31:15 +0000

    feat: app async (numeri primi non sequenziali)

--- first-app-async/app/index.js
+++ first-app-async/app/index.js
@@ -1,9 +1,22 @@
 const mathUtils = require("./math-utils");
 
 let i = 1;
-
+const range = 50;
+let current = 0;
+const end = 5000;
+let result = []
 otherIncomingRequests();
-longRequest(50000);
+
+const reqInterval = setInterval(()=> {
+
+    result.push(...longRequestNew(current,current + range ));
+  current += range;
+  if (current >= end) {
+    clearInterval(reqInterval);
+    console.log('result-->', result);
+  }
+}, 150);
+
 
 /**
  * DO NOT CHANGE IT
@@ -12,6 +25,20 @@ function otherIncomingRequests() {
     setInterval(() => {
         console.log(`Id: ${i++}. Doing new incoming request`);
     }, 50);
+}
+
+
+function longRequestNew(from, to) {
+    let id = i++;
+    console.log(`Id: ${id}. Starting blocking request. Find primes from ${from} to ${to}`);
+    const start = new Date();
+
+    const primes = mathUtils.getPrimeNumbersWithinRange(from, to);
+
+    const end = new Date();
+    console.log(`Id: ${id}. Finished blocking request. Elapsed ms: ${end.getTime() - start.getTime()}`);
+
+    return primes;
 }
 
 /**
